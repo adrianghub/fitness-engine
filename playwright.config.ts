a5f3322cc5f4c6a3abed32a390506e9b9485f0cb
Adrian Zinko <zinko.adrian00@gmail.com>
2025-05-04 18:56:26 +0200

    imp(tests/e2e): update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
   webServer: {
     command: "pnpm run dev",
     url: "http://localhost:5173",
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
     stdout: "pipe",
     stderr: "pipe",
     timeout: 60000,
